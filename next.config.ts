1f9f707edaf3573c6b1e9df75693661103826a12
Elcy19 <ichmellatognide@gmail.com>
2026-02-17 11:25:54 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // ✅ Empêche ESLint de bloquer le build
+  },
 
-  },
-    experimental: {
-    nodeMiddleware: true, // <-- Permet d'utiliser runtime: 'nodejs' pour middleware
-  },
-   allowedDevOrigins: ['http://localhost:10000', 'https://mentiscare.onrender.com'],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
